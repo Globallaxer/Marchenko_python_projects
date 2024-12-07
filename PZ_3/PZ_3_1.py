d8480dbc8914778421cd23c2524617fc7662154b
Globallaxer <globallaxer@gmail.com>
2024-12-07 14:09:31 +0300

    Сделано пз 3.1

---
+++ PZ_3/PZ_3_1.py
@@ -0,0 +1,25 @@
+#1. Дано целое число. Если оно является положительным, то прибавить к нему 1; если
+# отрицательным, то вычесть из него 2; если нулевым, то заменить его на 10. Вывести
+# полученное число.
+#2. Даны три целых числа. Найти количество положительных и количество
+# отрицательных чисел в исходном наборе.
+while True:
+
+    a = input("Введите целое число")
+    try:
+        number_a = int(a)
+        if number_a == 0:
+            number_a = 10
+            print(number_a)
+            break
+        elif number_a < 0 :
+            number_a = number_a - 2
+            print(number_a)
+            break
+        elif number_a > 0:
+            number_a = number_a + 1
+            print(number_a)
+            break
+    except ValueError:
+        print("Ошибка, введите целое число")
+
