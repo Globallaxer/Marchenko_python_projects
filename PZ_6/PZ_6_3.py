871f848f0ac94bd63b75047b8cd0b6671e92dbfe
Globallaxer <globallaxer@gmail.com>
2024-12-14 09:28:17 +0000

    Сделана PZ_6_3

---
+++ PZ_6/PZ_6_3.py
@@ -0,0 +1,25 @@
+# Дан список размера N. Осуществить циклический сдвиг элементов списка вправо на
+# одну позицию (при этом A1 перейдет в A2, A2 — в A3,..., AN — в A1).
+import random 
+
+while True:
+    try:
+        a = []
+        n = int(input("b "))
+        i = 1
+        while i < n:
+            b = random.randint(0, 10)
+            a.append(b)
+            i += 1
+        print(f'Созданный список: {a}')
+        last_fragment = a[-1]
+        for d in range(len(a) - 1,0,-1):
+            a[d] = a[d - 1]
+        a[0] = last_fragment
+
+        print(f"Список после циклического сдвига - {a}")               
+            
+        break
+
+    except ValueError:
+        print("Ошибка, введите число")
