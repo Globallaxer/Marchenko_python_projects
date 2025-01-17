db58a30ea7d9d786b1ba33d948affe621613ba66
Globallaxer <globallaxer@gmail.com>
2025-01-17 12:33:58 +0000

    Сделана PZ_7_2

--- PZ_7/PZ_7_1.py
+++ PZ_7/PZ_7_1.py
@@ -1,17 +1,17 @@
 # Дана строка. Подсчитать количество содержащихся в ней прописных латинских букв.
-while True:
-    try:
-        s = input("Введите строку = ")
-        i = 1 
-        otvet = 0
-        for i in s:
-            n = int(ord(i))
-            if 97 <= n <= 122:
-                otvet += 1
-        print(f'кол-во латинских прописных букв в этой строке = {otvet}')
-        break        
+# while True:
+#     try:
+#         s = input("Введите строку = ")
+#         i = 1 
+#         otvet = 0
+#         for i in s:
+#             n = int(ord(i))
+#             if 97 <= n <= 122:
+#                 otvet += 1
+#         print(f'кол-во латинских прописных букв в этой строке = {otvet}')
+#         break        
 
-    except ValueError:
-        print("Ошибка, Введите строку")
-
+#     except ValueError:
+#         print("Ошибка, Введите строку")
+print(ord('ы'))
                
