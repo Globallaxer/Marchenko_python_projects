a9be132d05d38da93445a55da78a014b65147b0e
Globallaxer <globallaxer@gmail.com>
2025-01-24 08:48:53 +0000

    доделана pz_7_2

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -1,2 +1,2 @@
 avto = {'BMW':['XM', 'X7', 'X6 M'],'Shevrolet':['Apache','Astra','Aveo'],'lada':['Granta Седан','Granta лифтбек','Granta Cross'] }
-print(f'2 модель BMW = {avto['BMW'][1]}, 2 модель Shevrolet ={avto["Shevrolet"][1]},2 модель lada ={avto["lada"][1]}')
+print(f'2 модель BMW = {avto['BMW'][1]}, 2 модель Shevrolet = {avto["Shevrolet"][1]},2 модель lada = {avto["lada"][1]}')
