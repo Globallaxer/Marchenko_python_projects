e91df6987f392acdc295ec37a198ad8fde3a9884
Globallaxer <globallaxer@gmail.com>
2025-03-01 09:53:03 +0000

    52

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -2,6 +2,17 @@
 # количество пробельных символов. Сформировать новый файл, в который поместить текст
 # в стихотворной форме предварительно заменив символы третей строки их числовыми
 # кодами.
-f1 = open('text18-14.txt', encoding='UTF-8')
-l = f1.read
-print(l)
+with open('text18-14.txt','r',encoding='utf-8') as file:
+    content = file.readlines()
+with open('text18-14.txt','r',encoding='utf-8') as file:
+    content_for_spaces = file.read()
+    spaces = content_for_spaces.count(' ')
+    print(f'Абоба, вот твои пробелы: {spaces}')   
+    for line in content:
+        print(line,end="")
+    if len(content) >= 3:
+        line_ascii = [str(ord(char)) for char in content[2]]
+        content[2] = ' '.join(line_ascii)+ "\n"
+    with open("text18-14_mod.txt",'w', encoding='utf-8') as new_file:
+        new_file.writelines(content)
+        
