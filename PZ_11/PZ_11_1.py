f5e00eae493bfd920a4cbc93fe57a285dce15f5e
Globallaxer <globallaxer@gmail.com>
2025-04-10 20:11:57 +0000

    52

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -5,11 +5,37 @@
 # обработку элементов:
 # Исходные данные:
 # Количество элементов:
-# Индекс первого минимального элемента:
+# Индекс  минимального элемента:
 # Умножаем все элементы на минимальный элемент:
 import random
+vvod = int(input('Введите желаемое количество элементов - '))
+sequence = [random.randint(-100, 100) for _ in range(vvod)]
 
-vvod = int(input('Введите желаемое кол-во элементов - '))
-random_vvode = [random.randint(-100, 100) for i in range(vvod)]
-with open('data_3.txt','w', encoding='UTF-8') as file:
-    file.writelines(random_vvode)
+
+with open('data_3.txt', 'w') as file:
+    for num in sequence:
+        file.write(str(num) + '\n')
+
+
+with open('data_3.txt', 'r') as file:
+    numbers = [int(line.strip()) for line in file.readlines()]
+
+
+
+min_value = min(numbers)
+min_index = numbers.index(min_value)
+
+
+multiplied_sequence = [num * min_value for num in numbers]
+
+
+with open('data_4.txt', 'w') as file:
+    file.write("Исходные данные:\n")
+    file.write(str(numbers) + '\n\n')
+    file.write("Количество элементов: " + str(len(numbers)) + '\n\n')
+    file.write("Минимальный элемент: " + str(min_value) + '\n')
+    file.write("Индекс минимального элемента: " + str(min_index) + '\n\n')
+    file.write("Умножаем все элементы на минимальный элемент:\n")
+    file.write(str(multiplied_sequence))
+
+
