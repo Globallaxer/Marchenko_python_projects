25ced8b62c75b972048b6d218349ec5fa351a02a
Globallaxer <globallaxer@gmail.com>
2025-04-25 13:07:29 +0000

    52

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -3,10 +3,11 @@ import random
 matrix = [[random.randint(-10, 10) for _ in range(4)] for _ in range(4)]
 print(f"Исходная матрица: {matrix}")
 total_sum = 0
-for i in range(len(matrix[0])):
-        if (i + 1) % 2 == 0: 
-            col_sum = sum(row[i] for row in matrix)
-            total_sum += col_sum
-            print(f"Сумма в столбце {i + 1}: {col_sum}")
+column_sums = list(map(
+    lambda col: sum(col),
+    [column for i, column in enumerate(zip(*matrix)) if (i+1) % 2 == 0]
+))
+
+print(column_sums)
 
 
