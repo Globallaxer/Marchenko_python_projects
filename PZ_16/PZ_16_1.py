3e595423ca17b2da81b3861f3b6c7184caadcaf7
Laxer <globallaxer@gmail.com>
2025-06-06 09:40:47 +0000

    52

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -3,11 +3,13 @@
 # марка, Модель: модель, Год выпуска: год".
 
 class car :
-    def __init__(self,marl,model,year):
-        self.marl=marl
-        self.model=model
-        self.year=year
+    def __init__(self, marl, model, year):
+        self.marl = marl
+        self.model = model
+        self.year = year
+
     def vsvod(self):
-        print(f'Марка:{self.marl},Модель={self.model},Год выпуска = {self.year}')
-car1 = car("BMW","X5","2015")     
-car1.vsvod   
+        print(f'Марка: {self.marl}, Модель: {self.model}, Год выпуска: {self.year}')
+
+car1 = car("BMW","X5",2015)     
+car1.vsvod()   
